d73b89bfe6222244ebca0aa5c082f0e2cc3392c0
2myaus <twomyaus@gmail.com>
2025-06-03 11:58:28 -0400

    first

---
+++ Makefile
@@ -0,0 +1,64 @@
+# ty nota <3
+
+# Output binary
+BIN = DiscordFortuneCowsay
+
+# Put all auto generated stuff to this build dir.
+BUILD_DIR = ./build
+
+SOURCE_DIR = src
+
+# List of all the source files.
+SOURCE = \
+	main.cpp \
+
+INCLUDES = \
+	src \
+
+CXX = g++
+
+CXX_FLAGS = \
+	-g -Wall -Wextra -Wfloat-equal -Wswitch-default -Wswitch-enum --std=c++20 \
+	${shell pkg-config --cflags dpp} \
+
+LNK_FLAGS = \
+	${shell pkg-config --libs dpp} \
+
+# All .o files go to build dir.
+CXX_OBJ = $(SOURCE:%.cpp=$(BUILD_DIR)/%.o)
+OBJ = $(CXX_OBJ)
+
+
+# Gcc/Clang will create these .d files containing dependencies.
+DEP = $(OBJ:%.o=%.d)
+# Include paths with a -I in front of them
+INCLUDEPATHS = $(INCLUDES:%= -I %)
+
+# Actual target of the binary - depends on all .o files.
+$(BIN) : PreBuild $(OBJ)
+	echo Linking...
+	# Create build directories - same structure as sources.
+	-mkdir -p $(@D) 2>/dev/null
+	$(CXX) $(OBJ) $(LNK_FLAGS) -o $(BIN)
+
+# Include all .d files
+-include $(DEP)
+
+# Build target for every single object file.
+# The potential dependency on header files is covered
+# by calling `-include $(DEP)`.
+$(BUILD_DIR)/%.o : $(SOURCE_DIR)/%.cpp
+	echo Compiling $(notdir $<)
+	-mkdir -p $(@D) 2>/dev/null
+	# The -MMD flags additionaly creates a .d file with
+	# the same name as the .o file.
+	$(CXX) $(CXX_FLAGS) $(INCLUDEPATHS) -MMD -c $< -o $@
+
+
+.PHONY : clean
+clean:
+	-rm -r $(BUILD_DIR)
+	-rm $(BIN)
+
+.PHONY: PreBuild
+PreBuild:
---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include <cstdlib>
+#include <dpp/dpp.h>
+#include <format>
+
+// https://stackoverflow.com/a/478960
+std::string exec(std::string cmd) {
+  std::array<char, 128> buffer;
+  std::string result;
+  std::unique_ptr<FILE, void (*)(FILE *)> pipe(popen(cmd.c_str(), "r"),
+                                               [](FILE *f) -> void {
+                                                 // wrapper to ignore the return
+                                                 // value from pclose() is
+                                                 // needed with newer versions
+                                                 // of gnu g++
+                                                 std::ignore = pclose(f);
+                                               });
+  if (!pipe) {
+    throw std::runtime_error("popen() failed!");
+  }
+  while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe.get()) !=
+         nullptr) {
+    result += buffer.data();
+  }
+  return result;
+}
+
+int main() {
+
+  dpp::cluster bot(std::getenv("BOT_TOKEN"));
+
+  bot.on_slashcommand([](auto event) {
+    if (event.command.get_command_name() != "cowsay") {
+      return;
+    }
+    std::string cmdout = exec("fortune|cowsay");
+    event.reply(std::format("```\n{}\n```", cmdout));
+  });
+
+  bot.on_ready([&bot](auto event __attribute__((unused))) {
+    if (dpp::run_once<struct register_bot_commands>()) {
+      bot.global_command_create(
+          dpp::slashcommand("cowsay", "tylko jedno w głowie mam", bot.me.id));
+    }
+  });
+
+  bot.start(dpp::st_wait);
+  return 0;
+}
